f9748f592124eb56854bf7dbc8460daf407a2b95
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-06-27 09:55:09 +0800

    Load unified metric card styles

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,7 @@ import './stats-fix.css'
 import './corresponding-badge.css'
 import './mobile-metrics.css'
 import './mobile-top-3row.css'
+import './unified-metrics.css'
 
 function AppContent() {
   const { user, loading } = useAuth()
