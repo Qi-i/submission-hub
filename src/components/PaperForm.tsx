2ab20e7b200fd1d3a3ce9ffe874ea00eef2e8c44
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-10 17:13:16 +0800

    Inject cloud upload only in online paper form

--- src/components/PaperForm.tsx
+++ src/components/PaperForm.tsx
@@ -1,2 +1,9 @@
+import type { ComponentProps } from 'react'
+import { uploadFile } from '../lib/storage'
 import PaperFormArchive from './PaperFormArchive'
-export default PaperFormArchive
+
+type Props = Omit<ComponentProps<typeof PaperFormArchive>, 'onUploadFile'>
+
+export default function PaperForm(props: Props) {
+  return <PaperFormArchive {...props} onUploadFile={uploadFile} />
+}
