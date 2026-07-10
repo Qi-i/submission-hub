0667dbea3e82c97c0d11cd29772e7ac6c2e1eab9
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-10 17:50:37 +0800

    Load preparation workspace styles offline

--- src/offline.tsx
+++ src/offline.tsx
@@ -18,6 +18,7 @@ import './final-polish.css'
 import './contrast-polish.css'
 import './timeline-today.css'
 import './action-center.css'
+import './preparation.css'
 import './logo-redesign.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
