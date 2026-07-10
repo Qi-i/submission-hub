d33aaf501578a09630efde0207c4acdf73192b9e
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-10 19:05:27 +0800

    Enable EasyScholar lookup in online paper form

--- src/components/PaperForm.tsx
+++ src/components/PaperForm.tsx
@@ -1,9 +1,10 @@
 import type { ComponentProps } from 'react'
+import { lookupJournalRanks } from '../lib/journal-rank-client'
 import { uploadFile } from '../lib/storage'
 import PaperFormArchive from './PaperFormArchive'
 
-type Props = Omit<ComponentProps<typeof PaperFormArchive>, 'onUploadFile'>
+type Props = Omit<ComponentProps<typeof PaperFormArchive>, 'onUploadFile' | 'onLookupJournalRanks'>
 
 export default function PaperForm(props: Props) {
-  return <PaperFormArchive {...props} onUploadFile={uploadFile} />
+  return <PaperFormArchive {...props} onUploadFile={uploadFile} onLookupJournalRanks={lookupJournalRanks} />
 }
