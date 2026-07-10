0ef70fe2025be7f371d9f910a1a4390da7089f11
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-10 17:37:56 +0800

    Improve offline browser compatibility and relative paths

--- vite.config.offline.ts
+++ vite.config.offline.ts
@@ -4,12 +4,14 @@ import { viteSingleFile } from 'vite-plugin-singlefile'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: './',
   plugins: [react(), viteSingleFile()],
   build: {
-    target: 'esnext',
+    target: 'es2020',
     assetsInlineLimit: 100000000,
     cssCodeSplit: false,
     outDir: 'dist-offline',
+    emptyOutDir: true,
     rollupOptions: {
       input: resolve(__dirname, 'offline.html'),
     },
