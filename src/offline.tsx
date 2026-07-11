67ef22d14ea6c3921a470105cef77b145a7ffec2
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-11 20:10:45 +0800

    Enable APC RMB conversion in offline build

--- src/offline.tsx
+++ src/offline.tsx
@@ -3,10 +3,12 @@ import ReactDOM from 'react-dom/client'
 import './app-styles'
 import { ThemeProvider } from './lib/theme'
 import OfflineDashboard from './components/OfflineDashboard'
+import ApcAutoConverter from './components/ApcAutoConverter'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
+      <ApcAutoConverter />
       <OfflineDashboard />
     </ThemeProvider>
   </React.StrictMode>,
