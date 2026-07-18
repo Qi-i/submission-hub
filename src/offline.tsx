40e879859d72aec93c63769fba09abd1f6646ffd
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-18 23:56:36 +0800

    Add offline first-run guide

--- src/offline.tsx
+++ src/offline.tsx
@@ -4,12 +4,14 @@ import './app-styles'
 import { ThemeProvider } from './lib/theme'
 import OfflineDashboard from './components/OfflineDashboard'
 import ApcAutoConverter from './components/ApcAutoConverter'
+import { OfflineFirstRunGuideGate } from './components/FirstRunGuide'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
       <ApcAutoConverter />
       <OfflineDashboard />
+      <OfflineFirstRunGuideGate />
     </ThemeProvider>
   </React.StrictMode>,
 )
