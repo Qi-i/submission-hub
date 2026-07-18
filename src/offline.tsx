865d2311d572943378b46be03f25aca89a5c67f0
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-19 00:14:20 +0800

    Isolate offline onboarding from account code

--- src/offline.tsx
+++ src/offline.tsx
@@ -4,14 +4,14 @@ import './app-styles'
 import { ThemeProvider } from './lib/theme'
 import OfflineDashboard from './components/OfflineDashboard'
 import ApcAutoConverter from './components/ApcAutoConverter'
-import { OfflineFirstRunGuideGate } from './components/FirstRunGuide'
+import OfflineFirstRunGuide from './components/OfflineFirstRunGuide'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
       <ApcAutoConverter />
       <OfflineDashboard />
-      <OfflineFirstRunGuideGate />
+      <OfflineFirstRunGuide />
     </ThemeProvider>
   </React.StrictMode>,
 )
