8e21a10bcf04f86a3bd8c63b6a328ebd6a3ae550
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-19 23:03:53 +0800

    Remember the last offline workspace

--- src/offline.tsx
+++ src/offline.tsx
@@ -5,11 +5,13 @@ import { ThemeProvider } from './lib/theme'
 import OfflineDashboard from './components/OfflineDashboard'
 import ApcAutoConverter from './components/ApcAutoConverter'
 import OfflineFirstRunGuide from './components/OfflineFirstRunGuide'
+import NavigationMemory from './components/NavigationMemory'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ThemeProvider>
       <ApcAutoConverter />
+      <NavigationMemory scope="offline" />
       <OfflineDashboard />
       <OfflineFirstRunGuide />
     </ThemeProvider>
