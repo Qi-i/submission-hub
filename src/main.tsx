933fbb2d678fb8c171a7e167500826630ebee363
Zhiqi Liu <118270330+Qi-i@users.noreply.github.com>
2026-07-25 00:08:24 +0800

    Load journal regression stabilizer

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './app-styles'
+import './journal-library-runtime-fixes'
 import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
